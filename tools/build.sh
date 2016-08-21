2cc8214b975577973febec4d22f7d3f22981b6b5
Morgan Robertson <robertson.md7@gmail.com>
2016-08-20 23:18:45 -0400

    Add build file for tools image.

---
+++ tools/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+    echo "This script requires root privileges"
+    exit 1
+fi
+
+acbuild --debug begin
+trap "{ acbuild --debug end && exit 1; }" EXIT
+
+acbuild --debug set-name mrgnr.io/tools
+acbuild --debug dep add quay.io/coreos/alpine-sh
+
+acbuild --debug run -- apk update
+acbuild --debug run -- apk add bash curl htop net-tools nload nmap python py-pip strace tcpdump \
+    tcptraceroute the_silver_searcher tree
+acbuild --debug run -- pip install httpie
+
+acbuild --debug set-exec -- /bin/sh -c /bin/bash
+
+acbuild --debug write --overwrite tools.aci
